d2a92927a251418e3104356dd626e2ffe77c7d2d
Ovidiu Donciu <ovidiudonciu@Ovidius-MacBook-Air.local>
2025-05-07 18:45:13 -0400

    final version

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * Entry point for GetYourTester application
+ * This file serves as the main entry point for production deployments
+ * It simply requires the webhook-server module which contains all the application logic
+ */
+
+// Import the main application
+require('./webhook-server.js'); 
